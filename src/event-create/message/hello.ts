3e05b764aee5339a89a778c62219d0c098209272
nopeless <38830903+nopeless@users.noreply.github.com>
2023-05-25 23:03:39 +0900

    edit hello message

--- src/event-create/message/hello.ts
+++ src/event-create/message/hello.ts
@@ -3,8 +3,9 @@ import { createChatReply } from "../../event";
 export default createChatReply({
   name: `hello`,
   scope: [`DM`],
-  exec(message) {
-    if (message.author.id !== this.ownerID) return;
-    message.reply(`hi!, command was ${message.prefixlessContent}`);
+  async exec(message) {
+    if (message.prefixlessContent !== `hello`) return;
+    // TODO add variety
+    await message.reply(`hi there!`);
   },
 });
