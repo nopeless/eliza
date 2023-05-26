5d308f8fdaf217fcdf7cffa300cb701455f729f4
nopeless <38830903+nopeless@users.noreply.github.com>
2023-05-26 23:37:42 +0900

    refactor cli logic

--- test/cli.ts
+++ test/cli.ts
@@ -1,43 +1,61 @@
 import { consola } from "consola";
-import { isCancel } from "@clack/core"
+import { dedent } from "@/lib/util";
+import { client } from "./fixtures";
 
-async function main(): Promise<false |void> {
-  const input = await consola.prompt("Enter a string"); 
+async function main(): Promise<false | void> {
+  const input = await consola.prompt(`Enter a string`, {
+    initial: `eliza `,
+  });
 
-  if (isCancel(input)) {
-    consola.info("^C - exit");
+  // XXX This is a trash way of detecting, but blame unjs people
+  if (typeof input === `symbol`) {
+    consola.info(`^C - exit`);
     return false;
   }
 
-  if (typeof input !== 'string') {
+  if (typeof input !== `string`) {
     console.warn(input);
     return;
   }
 
   if (input.match(/^[.!]?(?:exit|!?q(?:uit)?)$/)) {
-    consola.info("Exiting...");
+    consola.info(`Exiting...`);
     return false;
+  }
+
+  // actually run input
+  const response = await client.send(input);
+
+  if (response.mockReplies.length === 0) {
+    consola.info(`No reply`);
+  } else {
+    consola.info(response.mockReplies[0]);
   }
 }
 
 function intro() {
-  consola.info(`
+  consola.info(
+    `\n ---- ` +
+      dedent`
     Interactive eliza cli
     This simulates Discord chat without having to run the bot
 
-    By default, all channels are active
-  `)
+    By default, all channels are active`
+  );
 }
 
 (async () => {
   intro();
 
-  while (true) {
+  for (;;) {
     const r = await main();
     if (r === false) {
-      break
+      break;
     }
-  }  
-})().catch(e => {
-   console.error(e);
+  }
+
+  await client.destroy();
+  consola.log(`Gracefully exited`);
+})().catch((e) => {
+  console.error(e);
 });
