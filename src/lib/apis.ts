20ff4262731637a611bf5456900fc590fee6939b
nopeless <38830903+nopeless@users.noreply.github.com>
2023-06-04 16:11:43 +0900

    create quote api

--- src/lib/apis.ts
+++ src/lib/apis.ts
@@ -35,3 +35,23 @@ export async function defineWord(word: string) {
     definition: obj.definitions?.[0].definition,
   };
 }
+
+const quoteExample = {
+  _id: `Npg765fDCW9b`,
+  content: `Through pride we are ever deceiving ourselves. But deep down below the surface of the average conscience a still, small voice says to us, something is out of tune.`,
+  author: `Carl Jung`,
+  tags: [`Famous Quotes`],
+  authorSlug: `carl-jung`,
+  length: 163,
+  dateAdded: `2020-09-09`,
+  dateModified: `2023-04-14`,
+};
+
+type Quote = typeof quoteExample;
+
+export async function getRandomQuote() {
+  const res = await fetch(`https://api.quotable.io/random`);
+  const obj = (await res.json()) as Quote;
+
+  return obj;
+}
