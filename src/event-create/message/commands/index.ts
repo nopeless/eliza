2ac1361130517487b4b32da2a21c092e732b69ed
nopeless <38830903+nopeless@users.noreply.github.com>
2023-06-19 22:13:23 +0900

    create eval

--- src/event-create/message/commands/index.ts
+++ src/event-create/message/commands/index.ts
@@ -1,3 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { inspect } from "util";
 import { createChatReply } from "../../../event";
 import { train } from "../../../lib/doc";
 
@@ -31,4 +33,26 @@ export default {
       return `🎉`;
     },
   }),
+  // eval: createChatReply({
+  //   name: `eval`,
+  //   description: `evaluates javascript code`,
+  //   async exec(message) {
+  //     if (!message.prefixlessContent.match(/^eval/i)) return;
+  //     if (!this.hell.can(message.author, `exitApplication`))
+  //       return `You don't have permissions`;
+
+  //     // double safety
+  //     if (this.ownerID !== message.author.id)
+  //       return `You don't have permissions`;
+
+  //     // eslint-disable-next-line @typescript-eslint/no-this-alias, @typescript-eslint/no-unused-vars
+  //     const client = this; // @ts-ignore
+
+  //     let result: unknown = await eval?.(message.prefixlessContent.slice(4));
+
+  //     if (typeof result !== `string`) result = inspect(result);
+
+  //     return `\`\`\`js\n${result}\n\`\`\``;
+  //   },
+  // }),
 } as const;
