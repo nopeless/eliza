c0a725d5bd91d8a05357ee3ec51ab71df053c6e2
nopeless <38830903+nopeless@users.noreply.github.com>
2023-06-28 01:55:40 +0900

    loader

--- bite-me-im-making-my-own-loader.js
+++ bite-me-im-making-my-own-loader.js
@@ -5,10 +5,17 @@ import { resolve as resolveExtensionless } from "extensionless";
  * https://stackoverflow.com/a/68621282/10629176
  */
 const resolveSpecifierJs = (specifier) => {
-  return !isBuiltin(specifier) &&
-    specifier.match(/\/spica-node\/cache\/[\d\w]+$/)
-    ? `${specifier}.js`
-    : specifier;
+  if (isBuiltin(specifier)) return specifier;
+
+  const [_, pkgName, moduleName] =
+    specifier.match(/^(spica-node)\/(\w+)$/) ?? [];
+
+  if (!pkgName || !moduleName) return specifier;
+
+  console.log(`converted to`, `${pkgName}/dist/${moduleName}.js`);
+
+  return specifier;
+  // return `${pkgName}/dist/${moduleName}.js`;
 };
 
 export const resolve = async (specifier, context, nextResolve) => {
